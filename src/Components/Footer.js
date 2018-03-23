4cf1bd82b7fb284e74c1221755fa5a4799c2f27e
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-03-23 11:30:16 -0600

    footer with hidden admin button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div>
-            <h1>FOOTER</h1>
-            <a href={process.env.REACT_APP_LOGIN}><button>Admin</button></a>
+        <div className='footer'>
+            <span>Educating Humans Since 2018</span>
+            <a href={process.env.REACT_APP_LOGIN}>Admin</a>
         </div>
     )
 }
