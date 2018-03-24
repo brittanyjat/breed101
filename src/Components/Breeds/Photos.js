8ca9cff76ee4b8fe951740853746fbaabecc77a5
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-03-24 14:25:26 -0600

    polishing

--- src/Components/Breeds/Photos.js
+++ src/Components/Breeds/Photos.js
@@ -36,7 +36,7 @@ class Photos extends Component {
         const { photos } = this.props;
         const carouselOptions = photos.map((pic, i) => {
             return (
-                <img src={pic} key={i} alt='breed' style={{maxHeight: '90%'}}/>
+                <img src={pic} key={i} alt='breed' style={{maxHeight: '80%'}}/>
             )
         })
         return (
