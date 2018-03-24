5a3cd84b1da775210d759edef32f8a024698d48c
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-03-24 13:17:36 -0600

    about

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,26 +6,26 @@ export default function About() {
         <div>
             <MainHeader />
             <div className='about-bottom'>
-                <h2>ABOUT THE SITE</h2>
+                <h2>ABOUT</h2>
                 <h4>The sole purpose for this site is to showcase what I have learned while attending DevMountain so far.</h4>
 
                 <p>I take no credit for any of the information on this site, as it is for educational purposes only.</p>
-                <ul className='about-list'>
+                <div className='about-list'>
                     <h5>SOURCES</h5>
-                    <a href='http://www.akc.org/' target='_blank' rel="noopener noreferrer"><li>American Kennel Club</li></a>
-                    <a href='http://dogtime.com/' target='_blank' rel="noopener noreferrer"><li>Dog Time</li></a>
-                    <a href='http://www.petwave.com/' target='_blank' rel="noopener noreferrer"><li>Pet Wave</li></a>
-                    <a href='http://www.pexels.com/' target='_blank' rel="noopener noreferrer"><li>Pexels</li></a>
-                    <a href='http://www.unsplash.com/' target='_blank' rel="noopener noreferrer"><li>Unsplash</li></a>
-                </ul>
-                <ul className='about-list'>
+                    <a href='http://www.akc.org/' target='_blank' rel="noopener noreferrer"><h6>American Kennel Club</h6></a>
+                    <a href='http://dogtime.com/' target='_blank' rel="noopener noreferrer"><h6>Dog Time</h6></a>
+                    <a href='http://www.petwave.com/' target='_blank' rel="noopener noreferrer"><h6>Pet Wave</h6></a>
+                    <a href='http://www.pexels.com/' target='_blank' rel="noopener noreferrer"><h6>Pexels</h6></a>
+                    <a href='http://www.unsplash.com/' target='_blank' rel="noopener noreferrer"><h6>Unsplash</h6></a>
+                </div>
+                <div className='about-list'>
                     <h5>BUILT WITH</h5>
-                    <li>React</li>
-                    <li>Node</li>
-                    <li>Postgres</li>
-                    <li>Redux</li>
-                    <li>Dog CEO API</li>
-                </ul>
+                    <h6>React</h6>
+                    <h6>Node</h6>
+                    <h6>Postgres</h6>
+                    <h6>Redux</h6>
+                    <h6>Dog CEO API</h6>
+                </div>
             </div>
         </div>
     )
